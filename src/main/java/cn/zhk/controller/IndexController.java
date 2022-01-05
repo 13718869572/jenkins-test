1ee80f1eba38a30ee21b4964c716106b8656aee7
zhouhuakang <zhouhuakang@gome.com.cn>
2022-01-05 17:33:49 +0800

    新增

---
+++ src/main/java/cn/zhk/controller/IndexController.java
@@ -0,0 +1,16 @@
+package cn.zhk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+
+}
